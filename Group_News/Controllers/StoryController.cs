b86d0e8c3792ecdac31bdc19e9cb332fcf113fe2
Sparks <dsparks@proforma.com>
2018-04-30 14:05:40 -0400

    create api get for stories

---
+++ Group_News/Controllers/StoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using Group_News.Models;
+using Group_News.Context;
+
+namespace Group_News.Controllers
+{
+    public class StoryController : ApiController
+    {
+        //view all stories
+        [HttpGet]
+        [Route ("api/stories")]
+        public IEnumerable<Story> GetStories()
+        {
+            using(var db = new GroupNewsContext())
+            {
+                return db.Stories.OrderBy(o => o.Headline).ToList(); 
+            }
+        }
+
+
+    }
+}
