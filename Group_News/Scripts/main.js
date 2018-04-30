460dd560739656d3e34c168b9eb7f7f49fdaa43d
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-30 14:38:05 -0400

    added some javascript to get categories and stories

--- Group_News/Scripts/main.js
+++ Group_News/Scripts/main.js
@@ -4,4 +4,25 @@ angular
     .module("main", [])
     .controller("homeController", ["$scope", "$http", ($scope, $http) => {
         $scope.categories = [];
+        $scope.stories = [];
+
+        const getCategories = () => {
+            let catURL = "/api/categories"
+            $http({
+                method: "GET",
+                url: catURL
+            }).then(resp => {
+                $scope.categories = resp.data
+            })
+        }
+
+        const getStories = () => {
+            let storiesURL = "/api/stories"
+            $http({
+                method: "GET",
+                url: storiesURL
+            }).then(resp => {
+                $scope.stories = resp.data;
+            })
+        }
     }
